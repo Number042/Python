7bb98c4f23e8e5f7e99347f51cd0ee79126c4f95
Number042 <marian.lueckhof@cern.ch>
2018-11-23 16:48:41 +0100

    Modulization: unify all selection processes prior to plotting in class DataSelection(). All processing done on certain DF, final output groupby, that is used as input for plotting.

--- FCCee-Synchrotron-Radiation/source/OpticsSelectTools.py
+++ FCCee-Synchrotron-Radiation/source/OpticsSelectTools.py
@@ -4,143 +4,270 @@ import re
 class DataSelection:
     
     """
-    Tool-kit to refine selection on overall input data. 
+    Tool-kit to refine selection on overall input data. Takes one DF to be processed and this DF is then 
+    updated along the way.
     """
     
     def __init__(self, df, verbose):
         self.df = df
         self.verbose = verbose
         self._optics = []
+        self._collimation = 0
 
-    def opticsSelection(self, optics = 'all', verbose = 0):
+        # cleanup section: drop columns from received DF that are not required
+        self.df.drop( ['mass','seco_pos_eudata'], axis = 1, inplace = True)
+
+    # STEP 1 -- select a machine and eventually specific optics
+    #
+    def opticsSelection(self, optics = 'all' ):
         """
         Function to select specific optics, put these into a new dataframe that can be passed for further processing. 
-            -- optics:  choose which otics to be put into the new frame
+            -- optics:  choose which otics to be put into the new frame. Can be general or specific
             -- verbose: set output level for user information
-        RETURN: another frame, holding only data from selected optics
+        RETURN: another frame, holding only data from selected optics but still class object?
         """
         self._optics = optics
         if optics == []: raise ValueError('Optics list is empty.')
         
-        # set up default (empty) frame and list, looping specified optics and appending to new df
+        # set up empty list, looping specified optics and appending to self.df_opt
         #
-        df_opt = pd.DataFrame()
+        self.df_opt = pd.DataFrame()
         tmpList = []
 
-        if verbose: 
+        if self.verbose: 
             print ("-*-*-*-*-*-*-*-*-*-*-*-*-*- \n Following frame(s) selected:")
             for optic in self._optics: print("--", optic)
         
         for opt in self._optics: tmpList.append( self.df[self.df.optics.str.contains(opt)] )
+        
         if tmpList == []: raise RuntimeError('No data collected. List of frames empty:', tmpList)
-    
         else:
-            df_opt = df_opt.append(tmpList)
-            df_opt.name = self.df.name
-                
-        df_opt.name = self.df.name
-        return df_opt
+            self.df_opt = self.df_opt.append(tmpList)
+            self.df_opt.name = self.df.name
+
+        if re.findall('col', self.df_opt.name):
+            self._collimation = 1
+            print ("Found collimator frame - groupby 'CollDim' \n", "-----------------------------")
+        else:
+            self._collimation = 0
+            print ("Found no collimator frame - analysing default data \n", "-----------------------------")
         
     
-    def get_beamShapes_and_Size(self, df, verbose = 0):
-        """
-        Function to read all beam types and sizes from a given frame. Allows to check for plotting if desired 
-        type and/or size is/are available.
-            -- df:      dataframe to pass to this function
-            -- verbose: set verbosity level; defaults to 0
-        """
-        
-        # df = self.df
-        
-        beamTypes = set(df.BeamShape) 
-        beamSize = set(df.BeamSize)
-        
-        if verbose: print ("*-*-*-*-*-*-*-*-*-*-*-*-* \n Available types:", beamTypes, "\n Available sizes:", beamSize)
-        
-        return beamTypes, beamSize
+        # do the slicing based on Creator and charge to secure synchrotron radiation -- mlu 11-21-2017 now hardcoded!
+        #
+        self.df_opt = self.df_opt[(self.df_opt.Creator == 'SynRad') & (self.df_opt.charge == 0)]
+        self.df_opt.name = self.df.name
 
-    def splitNames(self, df, verbose = 0):
+        return self.df_opt
+
+    def splitNames( self ):
         """
         Function to split the content of the 'Name' column that is written in the default frame by 'readG4out' and 'opticsSelection'.
         It splits the strings of that column into 4 different columns for element, type, number and vacuum.
-            -- df:      user needs to pass in the data frame to be splitted. If just using self object, another frame would be created
-                        from class instance, regardless of possible previous optics selection.
-            -- verbose: set the verbosity level
-        RETURN: a frame with appended columns holding the splitted content
+
+        RETURNS: a frame with appended columns holding the splitted content
         """
-        
-        dfName = df.name
-        
-        if verbose == 1: 
-            print ("-*-*-*-*-*-*-*-*-*-*-*-*-*- \n Selected following data frame:", df.name)
-        elif verbose > 1:
-            print (df.head())
-            print (df.Name)
+        if self.verbose == 1: 
+            print ("-*-*-*-*-*-*-*-*-*-*-*-*-*- \n Selected following data frame:", self.df_opt.name)
+        elif self.verbose > 1:
+            print (self.df_opt.head())
+            print (self.df_opt.Name)
         
         # mlu -- 11-22-2018 -- new implementation of split names:
         # --> split 'Name' and reuse for shorter element name
         # --> split 'OrigVol' and replace with its plain name
         #
-        df['Name'], df['type'], df['vacuum'] = df['Name'].str.split('_', 3).str 
-        df['OrigVol'] = df['OrigVol'].str.split('_').str[0]
+        self.df_opt['Name'], self.df_opt['type'], self.df_opt['vacuum'] = self.df_opt['Name'].str.split('_', 3).str 
+        self.df_opt['OrigVol'] = self.df_opt['OrigVol'].str.split('_').str[0]
         
-        df.name = dfName
-        return df
-        
-    # Add function for aperture selection, pass this to plot_data if needed
-    # extend to more detailed data filtering?
-    #
-    def aper_select(self, aperture, verbose):
-        """
-        Function to do some data filtering.
-            -- aperture:    allows to choose only certain dimensions; provided by calling function
-            -- verbose:     set level of verbosity, also provided by calling function
-        
-        RETURN: dataframe grouped wrt to aperture selection
-        """
-        
-        df = self.df
-        dfName = df.name
-        
-        if verbose: print ( " -*-*-*-*-*-*-*-*-*-*-*-*- \n", dfName, "holds: \n", df.keys() )
-        
-        # create a list of available apertures from the frame
-        #
-        aperList = df.CollDim.unique()
-        if verbose: print ( " -*-*-*-*-*-*-*-*-*-*-*-*- \n", "List of apertures: ", aperList )
-        
-        # slice out SR data from overall frame df
-        # pass name from df to sliced df
-        #
-        df_sliced = df[(df.Creator == 'SynRad') & (df.charge == 0)]
-        df_sliced.name = dfName
+        return self.df_opt
     
-        # check the df name, if collimation data, groupby apertures
-        #
-        if re.findall('col', df_sliced.name):
-            print ("found collimator frame - groupby 'CollDim' \n ----------------------------- \n")
-            if verbose: print ("available dimensions:", aperList)
 
-        if aperture == 'all':
-            # add the aperture option here?
-            print ('selected all apertures!')
-            grouped = df_sliced.groupby(['CollDim', 'optics', 'BeamShape'])
-            
+    # STEP 2 -- check for collimation data and make first selection
+    # check for collimation and use switch for groupby further below
+    #
+    def elmSelection(self, elements = 'all'):
+        """
+        Function to select certain elements. Can be useful for plotting to demonstrate effects of or on
+        single Elements
+            -- elements:    list of names, defaults to all which leaves the frame untrouched
+        
+        RETURNS: sliced DF, according to element selection
+        """
+
+        # additional option to select certain element(s), has to be able to accept a list
+        #
+        if elements:
+            self.df_opt = self.df_opt[ self.df_opt.Name.isin(elements) ] 
         else:
+            print (" *** No elements selected, defaults to all.")
+        
+        # check result of selection above
+        #   
+        if self.verbose > 1: print ("Sliced data frame: \n", "----------------------------- \n", self.df_opt)
+        
+        # returns the UPDATED frame from opticsSelection!
+        return self.df_opt
+
+    # if verbose: print ("beam types:", self.beamTypes, '\n' "beam sizes:", self.beamSizes)
+    def get_beamShapes_and_Size(self):
+        """
+        Function to read all beam types and sizes from a given frame. Allows to check for plotting if desired 
+        type and/or size is/are available.
+
+        RETURNS: list of available beam types and another of beam sizes
+        """
+        
+        self.beamTypes = set(self.df_opt.BeamShape) 
+        self.beamSizes = set(self.df_opt.BeamSize)
+        
+        if self.verbose: print ( "*-*-*-*-*-*-*-*-*-*-*-*-* \n Available types:", self.beamTypes, "\n Available sizes:", self.beamSizes )
+        
+        # maybe not required to pass them out of the class?
+        return self.beamTypes, self.beamSizes
+
+    def sliceFrame( self, beam = 'all', size = 'all' ):
+        """
+        Function to do sophisticated slicing on the DF produced by opticsSelection. Has to be used as LAST STEP.
+            -- beam:    list of beam types, can be 'pencil', 'gauss', 'ring' or 'flat'; default 'all', accepts list
+            -- size:    list of beam sizes; default 'all', accepts list
+        
+        RETURNS: a groupby object, grouped by optics, beam-type and -size.
+        """
+        # case 1
+        #
+        if beam == 'all' and size == 'all' or size == []:
+                
+            print ('selected all beam types and sizes!')
+            
+            if self._collimation: grouped = self.df_opt.groupby(['CollDim','optics','BeamShape','BeamSize'])
+            else: grouped = self.df_opt.groupby(['optics','BeamShape','BeamSize']) 
+            
+        # case 2
+        #
+        elif beam == 'all' and size != 'all':
             DF = pd.DataFrame()
             
-            for i in aperture:
-                print ('aperture selected:', i)
-                if i in aperList:
-                    tmp = df_sliced[df_sliced.CollDim == i]
-                    # ~ hits = df_sliced[(df_sliced.) & ()]
+            for j in size:
+                print ("All beam types of size", j, "selected")
+                    
+                if j in self.beamSizes:
+                    tmp = self.df_opt[self.df_opt.BeamSize == j] 
                     DF = DF.append(tmp)
+                    if self.verbose: print (tmp.head())
                 else:
-                    raise ValueError('Selected aperture', i, 'not in the list. Available are:', aperList)
-                
-            grouped = DF.groupby(['CollDim', 'optics', 'BeamShape']) 
+                    raise KeyError("Selected beam size", j, "not in the list of available beam sizes:", self.beamSizes)
+            
+            if self._collimation: grouped = DF.groupby(['CollDim','optics','BeamSize'])
+            else: grouped = DF.groupby(['optics','BeamSize'])
 
+        # case 3    
+        #
+        elif beam != 'all' and size != 'all':
+            
+            DF = pd.DataFrame()
+            framelist = []
+            for i in beam:
+                for j in size:
+                    print ("Type", i, "selected, with size", j, "sigma")
+                        
+                    if i in self.beamTypes and j in self.beamSizes:
+                        tmp = self.df_opt[(self.df_opt.BeamShape == i) & (self.df_opt.BeamSize == j)]
+                        framelist.append(tmp)
+                        
+                        if self.verbose > 1: print (tmp)
+                    
+                    elif i not in self.beamTypes:
+                        raise KeyError("Selected beam type", i, "not in the list of available beams:", self.beamTypes)
+                    elif j not in self.beamSizes:
+                        raise KeyError("Selected beam size", j, "not in the list of available beam sizes:", self.beamSizes)
+            
+            DF = DF.append(framelist)
+            if self._collimation: grouped = DF.groupby(['CollDim','optics','BeamShape','BeamSize'])
+            else: grouped = DF.groupby(['optics','BeamShape','BeamSize'])
+
+        # case 4
+        #
+        elif beam != 'all' and size == 'all':
+            
+            DF = pd.DataFrame()
+            for i in beam:
+                print ("Type", i, "selected with all sizes.")
+                
+                if i in self.beamTypes:
+                    tmp = self.df_opt[self.df_opt.BeamShape == i] 
+                    DF = DF.append(tmp)
+                    #if self.verbose: print (tmp.head())
+                else:
+                    raise KeyError("Selected beam type", i, "not in the list of available beams:", self.beamTypes)
+            
+            if self._collimation: grouped = DF.groupby(['CollDim','optics','BeamShape']) #,'BeamSize'])
+            else: grouped = DF.groupby(['optics','BeamShape']) #,'BeamSize'])
+
+        else:
+            raise RuntimeError("Invalid selection of choice(s)!")
+
+        # definitely required to available outside --> plottig or analysis
         return grouped
 
+
+    
+    # # Add function for aperture selection, pass this to plot_data if needed
+    # # extend to more detailed data filtering?
+    # #
+    # def aper_select(self, aperture, verbose):
+    #     """
+    #     Function to do some data filtering.
+    #         -- aperture:    allows to choose only certain dimensions; provided by calling function
+    #         -- verbose:     set level of verbosity, also provided by calling function
+        
+    #     RETURN: dataframe grouped wrt to aperture selection
+    #     """
+        
+    #     df = self.df
+    #     dfName = df.name
+        
+    #     if verbose: print ( " -*-*-*-*-*-*-*-*-*-*-*-*- \n", dfName, "holds: \n", df.keys() )
+        
+    #     # create a list of available apertures from the frame
+    #     #
+    #     aperList = df.CollDim.unique()
+    #     if verbose: print ( " -*-*-*-*-*-*-*-*-*-*-*-*- \n", "List of apertures: ", aperList )
+        
+    #     # slice out SR data from overall frame df
+    #     # pass name from df to sliced df
+    #     #
+    #     self.df_opt = df[(df.Creator == 'SynRad') & (df.charge == 0)]
+    #     self.df_opt.name = dfName
+    
+    #     # check the df name, if collimation data, groupby apertures
+    #     #
+    #     if re.findall('col', self.df_opt.name):
+    #         print ("found collimator frame - groupby 'CollDim' \n ----------------------------- \n")
+    #         if verbose: print ("available dimensions:", aperList)
+
+    #     if aperture == 'all':
+    #         # add the aperture option here?
+    #         print ('selected all apertures!')
+    #         grouped = self.df_opt.groupby(['CollDim', 'optics', 'BeamShape'])
+            
+    #     else:
+    #         DF = pd.DataFrame()
+            
+    #         for i in aperture:
+    #             print ('aperture selected:', i)
+    #             if i in aperList:
+    #                 tmp = self.df_opt[self.df_opt.CollDim == i]
+    #                 # ~ hits = self.df_opt[(self.df_opt.) & ()]
+    #                 DF = DF.append(tmp)
+    #             else:
+    #                 raise ValueError('Selected aperture', i, 'not in the list. Available are:', aperList)
+                
+    #         grouped = DF.groupby(['CollDim', 'optics', 'BeamShape']) 
+
+    #     return grouped
+
+
+    
 
 
--- FCCee-Synchrotron-Radiation/source/PlotSelectTools.py
+++ FCCee-Synchrotron-Radiation/source/PlotSelectTools.py
@@ -6,27 +6,26 @@ class Tracking:
     Tool kit to do extended selection on tracking information from G4
     """
     
-    def __init__(self, verbose, collimation):
-        self.collimation = collimation
+    def __init__(self, verbose):
         self.verbose = verbose
         
-    def collectInfo(self, df, verbose):
+    def collectInfo(self, df ):
         """
         Method to collect basic information from the .out of G4 in arrays (basically for plotting)
             -- frame:   refers to the data frame object resulting from a groupby operation
         """
         
-        if verbose == 1: 
+        if self.verbose == 1: 
             print ("Selected frame contains: \n", " ------------------------ \n", df.head())
     
-        elif verbose > 1: 
+        elif self.verbose > 1: 
             print ("collectInfo(), line: ", sys._getframe().f_lineno)
             print ("Selected frame complete: \n", " ------------------------ \n", df)
         
         event_last = 999999999
         track_last = 999999999
  
-        # initiate all necessary arrays
+        # initiate all necessary arrays -- SLOW --> IMPROVE !!
         #
         Z_pos = []; Z_org = []; Z_hit = []; 
         E_org = []; E_hit = []
@@ -56,85 +55,9 @@ class Tracking:
                 E_hit.append(energ)
                 
                 
-        if verbose > 1: print("Collected data in \n", "Z_pos: \n ------------------------- \n", Z_pos, "\n Z_org: \n ------------------------- \n", Z_org, "\n Z_hit: \n ------------------------- \n", Z_hit) 
+        if self.verbose > 1: print("Collected data in \n", "Z_pos: \n ------------------------- \n", Z_pos, "\n Z_org: \n ------------------------- \n", Z_org, "\n Z_hit: \n ------------------------- \n", Z_hit) 
         
         # returns a tuple of lists
         #
         return Z_pos, Z_org, Z_hit, E_org, E_hit
 
-    def sliceFrame(self, df_sliced, beamTypes, beamSizes, beam, size, verbose):
-
-        # case 1
-        #
-        if beam == 'all' and size == 'all' or size == []:
-                
-            print ('selected all beam types and sizes!')
-            
-            if self.collimation: grouped = df_sliced.groupby(['CollDim','optics','BeamShape','BeamSize'])
-            else: grouped = df_sliced.groupby(['optics','BeamShape','BeamSize']) 
-            
-        # case 2
-        #
-        elif beam == 'all' and size != 'all':
-            DF = pd.DataFrame()
-            
-            for j in size:
-                print ("All beam types of size", j, "selected")
-                    
-                if j in beamSizes:
-                    tmp = df_sliced[df_sliced.BeamSize == j] 
-                    DF = DF.append(tmp)
-                    if self.verbose: print (tmp.head())
-                else:
-                    raise KeyError("Selected beam size", j, "not in the list of available beam sizes:", beamSizes)
-            
-            if self.collimation: grouped = DF.groupby(['CollDim','optics','BeamSize'])
-            else: grouped = DF.groupby(['optics','BeamSize'])
-
-        # case 3    
-        #
-        elif beam != 'all' and size != 'all':
-            
-            DF = pd.DataFrame()
-            framelist = []
-            for i in beam:
-                for j in size:
-                    print ("Type", i, "selected, with size", j, "sigma")
-                        
-                    if i in beamTypes and j in beamSizes:
-                        tmp = df_sliced[(df_sliced.BeamShape == i) & (df_sliced.BeamSize == j)]
-                        framelist.append(tmp)
-                        
-                        if self.verbose > 1: print (tmp)
-                    
-                    elif i not in beamTypes:
-                        raise KeyError("Selected beam type", i, "not in the list of available beams:", beamTypes)
-                    elif j not in beamSizes:
-                        raise KeyError("Selected beam size", j, "not in the list of available beam sizes:", beamSizes)
-            
-            DF = DF.append(framelist)
-            if self.collimation: grouped = DF.groupby(['CollDim','optics','BeamShape','BeamSize'])
-            else: grouped = DF.groupby(['optics','BeamShape','BeamSize'])
-
-        # case 4
-        #
-        elif beam != 'all' and size == 'all':
-            
-            DF = pd.DataFrame()
-            for i in beam:
-                print ("Type", i, "selected with all sizes.")
-                
-                if i in beamTypes:
-                    tmp = df_sliced[df_sliced.BeamShape == i] 
-                    DF = DF.append(tmp)
-                    #if self.verbose: print (tmp.head())
-                else:
-                    raise KeyError("Selected beam type", i, "not in the list of available beams:", beamTypes)
-            
-            if self.collimation: grouped = DF.groupby(['CollDim','optics','BeamShape']) #,'BeamSize'])
-            else: grouped = DF.groupby(['optics','BeamShape']) #,'BeamSize'])
-
-        else:
-            raise RuntimeError("Invalid selection of choice(s)!")
-
-        return grouped
