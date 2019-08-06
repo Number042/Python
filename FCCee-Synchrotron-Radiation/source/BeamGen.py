63196de390d5c1b9d8b7d707cc64752fb45f267b
Number042 <marian.lueckhof@cern.ch>
2019-08-06 13:33:44 +0200

    Basically ready to merge. Performance needs to be improved.
    	modified:   BeamGen.py: specify data types with numpy arrays initialization. Remove unneccessary variables where not required

--- FCCee-Synchrotron-Radiation/source/BeamGen.py
+++ FCCee-Synchrotron-Radiation/source/BeamGen.py
@@ -7,20 +7,19 @@ from Tools import sbplSetUp
 
 class Beam:
     
-    def __init__(self, beamFile, tfs, name, Npart, halfCross, pc, verbose = 0):
+    def __init__(self, beamFile, tfs, Npart, halfCross, pc, verbose = 0):
         
         self.beamFile = beamFile
         self.tfs = tfs
-        self.name = name
-        self.Npart = Npart = int(Npart)
+        self.Npart = Npart
         self.verbose = verbose
         self.HalfCross = halfCross
         self.pc = pc
 
         self.Eb = np.sqrt(pc**2 - (511e-6)**2)
 
-        if verbose: print("Created:", name, "\n    * Using file ", beamFile, "\n    * Number of particles:", Npart, '\n at energy Eb =', self.Eb)
-        
+        if verbose: print("Create beam. \n    * Using file ", beamFile, "\n    * Number of particles:", Npart, '\n at energy Eb =', self.Eb)
+
     def read_beam_size(self, dim):
         """
         Method to read in the beam gun information from the Fields_from_tfs output
@@ -29,23 +28,18 @@ class Beam:
         RETURNS: array with sig_xn, sig_xn', sig_yn, sig_yn', sig_z, sig_e
         """
         
-        beamFile = self.beamFile
-        beamsize = np.genfromtxt(beamFile, delimiter = None, skip_header = 2, max_rows = 1 )
+        beamsize = np.genfromtxt(self.beamFile, delimiter = None, skip_header = 2, max_rows = 1 )
         
         if self.verbose: print("Beam sizes:", beamsize)
         
         if dim == 'x': 
             if self.verbose: print("selected plane: ", dim, "sigma = ", beamsize[0] )
-            sqrtEmitX = beamsize[0]
-            return sqrtEmitX
+            return np.float32(beamsize[0])
         
         elif dim == 'y': 
             if self.verbose: print("selected plane: ", dim, "sigma = ", beamsize[2] )
-            sqrtEmitY = beamsize[2]
-            return sqrtEmitY
+            return np.float32(beamsize[2])
         
-        
-
     def set_gauss(self, plotPath, plot = 0, save = 0):
         """
         Method to plot a random Gaussian distribution in x,x' and y,y'.
@@ -56,20 +50,20 @@ class Beam:
 
         RETURNS: arrays for x,x' and y,y'
         """
-        if self.verbose: print ("generate Gaussian beam for:", self.name, " with ", self.Npart, " particles.")
+        if self.verbose: print ("generate Gaussian beam with ", self.Npart, " particles.")
         
         # read beam sizes from Fields_from_tfs output. Given is already the std deviation with sqrt(eps)
         #
         beamsizeX = self.read_beam_size('x'); 
-        beamsizeY = self.read_beam_size('y')   
+        beamsizeY = self.read_beam_size('y') 
 
         # horizontal plane                
-        self.BeamVecX = np.array([ rnd.gauss( 0, beamsizeX ) for i in range(self.Npart) ])
-        self.BeamVecXprim = np.array([ rnd.gauss( 0, beamsizeX ) for i in range(self.Npart) ])
+        self.BeamVecX = np.array([ rnd.gauss( 0, beamsizeX ) for i in range(self.Npart) ], dtype = np.float )
+        self.BeamVecXprim = np.array([ rnd.gauss( 0, beamsizeX ) for i in range(self.Npart) ], dtype = np.float )
 
         # vertical plane
-        self.BeamVecY = np.array([ rnd.gauss( 0, beamsizeY ) for i in range(self.Npart) ])
-        self.BeamVecYprim = np.array([ rnd.gauss( 0, beamsizeY ) for i in range(self.Npart) ])
+        self.BeamVecY = np.array([ rnd.gauss( 0, beamsizeY ) for i in range(self.Npart) ], dtype = np.float )
+        self.BeamVecYprim = np.array([ rnd.gauss( 0, beamsizeY ) for i in range(self.Npart) ], dtype = np.float )
         
         if self.verbose > 1: print ("BeamVecX =", self.BeamVecX, '\n', "BeamVecY =", self.BeamVecY)
         
@@ -101,12 +95,12 @@ class Beam:
         Phi = np.array([2*np.pi*i/self.Npart for i in range(self.Npart)])
         
         # horizontal plane
-        self.BeamVecX = np.array( [ np.abs(Nsig[0])*beamsizeX*np.cos(phi) for i,phi in zip(range(self.Npart), Phi) ] ) 
-        self.BeamVecXprim = np.array( [ np.abs(Nsig[0])*beamsizeX*np.sin(phi) for i,phi in zip(range(self.Npart), Phi) ] ) 
+        self.BeamVecX = np.array( [ np.abs(Nsig[0])*beamsizeX*np.cos(phi) for i,phi in zip(range(self.Npart), Phi) ], dtype = np.float ) 
+        self.BeamVecXprim = np.array( [ np.abs(Nsig[0])*beamsizeX*np.sin(phi) for i,phi in zip(range(self.Npart), Phi) ], dtype = np.float ) 
         
         # vertical plane
-        self.BeamVecY = np.array( [ np.abs(Nsig[1])*beamsizeY*np.cos(phi) for i,phi in zip(range(self.Npart),Phi) ] ) 
-        self.BeamVecYprim = np.array( [ np.abs(Nsig[1])*beamsizeY*np.sin(phi) for i,phi in zip(range(self.Npart),Phi) ] ) 
+        self.BeamVecY = np.array( [ np.abs(Nsig[1])*beamsizeY*np.cos(phi) for i,phi in zip(range(self.Npart),Phi) ], dtype = np.float ) 
+        self.BeamVecYprim = np.array( [ np.abs(Nsig[1])*beamsizeY*np.sin(phi) for i,phi in zip(range(self.Npart),Phi) ], dtype = np.float ) 
         
         if plot:
             plt.figure(figsize = (10, 10))
@@ -129,9 +123,7 @@ class Beam:
         RETURNS: array of beam energies of length Npart
         """
 
-        energy = np.array([ rnd.gauss(Edes, acceptance) for i in range(self.Npart) ])
-
-        return energy
+        return np.array([ rnd.gauss(Edes, acceptance) for i in range(self.Npart) ], dtype = np.float )
 
     def gen_BeamMom(self, elm):
         """
@@ -146,11 +138,11 @@ class Beam:
                            twiss.loc[twiss['NAME'] == elm]['ALFX'].item(), 
                            twiss.loc[twiss['NAME'] == elm]['ALFY'].item() )
         
-        vecsNCS = np.array( [np.array([self.BeamVecX[i], self.BeamVecXprim[i], self.BeamVecY[i], self.BeamVecYprim[i], 0, 0]) for i in range(self.Npart) ] )
+        vecsNCS = np.array( [np.array([self.BeamVecX[i], self.BeamVecXprim[i], self.BeamVecY[i], self.BeamVecYprim[i], 0, 0]) for i in range(self.Npart) ], dtype = np.float )
         
-        vecsCS = np.array([ FrmNrm @ vec for vec in vecsNCS ])
-        dirsCS = np.array([[vec[0], vec[2], np.sqrt(1 - vec[0]**2 - vec[2]**2)] for vec in vecsCS ])
-        dirsEU = np.array([ RotY(self.HalfCross, dirCS) for dirCS in dirsCS ])
+        vecsCS = np.array([ FrmNrm @ vec for vec in vecsNCS ], dtype = np.float )
+        dirsCS = np.array([[vec[0], vec[2], np.sqrt(1 - vec[0]**2 - vec[2]**2)] for vec in vecsCS ], dtype = np.float )
+        dirsEU = np.array([ RotY(self.HalfCross, dirCS) for dirCS in dirsCS ], dtype = np.float )
 
         return dirsEU
             
