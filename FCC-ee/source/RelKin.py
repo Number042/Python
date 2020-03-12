25f2f1728312334d9a22470a74cfc26042077a4d
Number042 <marian.lueckhof@cern.ch>
2020-03-12 09:56:08 +0100

    Update Gitignore. Shall ignore all Jupyter Notebooks
    	gelöscht:       source/BeamGen.py
    	gelöscht:       source/BeamParaSetup.py
    	gelöscht:       source/CS_to_EU.py
    	gelöscht:       source/DF_Tools.py
    	gelöscht:       source/Generators.py
    	gelöscht:       source/Input.py
    	gelöscht:       source/OpticsSelectTools.py
    	gelöscht:       source/PhotoInteract.py
    	gelöscht:       source/Plot.py
    	gelöscht:       source/PlotSelectTools.py
    	gelöscht:       source/RelKin.py
    	gelöscht:       source/ScoringPlot.py
    	gelöscht:       source/SynchrotronRadiation.py
    	gelöscht:       source/TfsTables.py
    	gelöscht:       source/ThrmlPht.py
    	gelöscht:       source/Tools.py
    	gelöscht:       source/VisualSpecs.py
    	gelöscht:       source/testPCBE.py

--- FCC-ee/source/RelKin.py
+++
@@ -1,7 +0,0 @@
-from numpy import array
-
-def Boost( gam, bet ):
-    return array( [ [gam, 0, 0, -bet*gam],
-                       [0, 1, 0, 0],
-                       [0, 0, 1, 0],
-                       [-bet*gam, 0, 0, gam] ] )
