8993edad86512dc91a1b66151d2c1c25bc227c42
Number042 <marian.lueckhof@cern.ch>
2020-08-09 16:08:33 +0200

    Improvements wrt beam parameters.
    	geändert:       BeamParaSetup.py -- update the parameter set and include cases to choose either parameters for synchrotron radiation (SR) or beamstrahlung (BS).

--- FCC-ee/source/BeamParaSetup.py
+++ FCC-ee/source/BeamParaSetup.py
@@ -1,31 +1,35 @@
 from numpy import sqrt
 class Parameters:
 
-    def __init__( self, pc ):
+    def __init__( self, pc, fRF, SRorBS ):
         
         self.pc = pc
         self.Eb = sqrt(pc**2 - (511e-6)**2)
+        self.fRF= fRF
+        self.SRorBS = SRorBS
 
         # print("summarize beam parameters for pc =", self.pc )
 
     def set_beam_parameters( self ):
 
         if self.pc == 45.6:
-            param = { 'Nbun':16640, 'Np':1.7e11, 'sigZ':3.5e-3, 'sigZ_BS':12.1e-3, 'epsX':.27e-9, 
+            param = { 'Nbun':16640, 'Np':1.7e11, 'sigZ':3.5e-3 if self.SRorBS == 'SR' else 12.1e-3, 'epsX':.27e-9, 
                        'epsY':1e-12, 'betStX':0.15, 'betStY':0.8e-3, 'sigStX':6.4e-6,
-                       'sigStY':28e-9, 'Lrnt':self.pc/511e-6, 'brho':10*self.pc/2.9979, 'Ib':1390, 'tau_BSP':19.6,  'sigmPBS':0.132, 'sigmPSR': 0.038 } 
+                       'sigStY':28e-9, 'Lrnt':self.pc/511e-6, 'brho':10*self.pc/2.9979, 'Ib':1390, 'tau_BSP':19.6,  
+                       'sigmP':0.132 if self.SRorBS == 'BS' else 0.038,
+                       'V_RF':0.1e9 } 
 
         elif self.pc == 182.5:
-            param = { 'Nbun':48, 'Np':2.3e11, 'sigZ':1.97e-3, 'sigZ_BS':2.54e-3, 'epsX':1.46e-9, 
+            param = { 'Nbun':48, 'Np':2.3e11, 'sigZ':1.97e-3 if self.SRorBS == 'SR' else 2.54e-3, 'epsX':1.46e-9, 
                       'epsY':2.9e-12, 'betStX':1., 'betStY':1.6e-3, 'sigStX':38.2e-6, 
-                      'sigStY':68e-9, 'Lrnt':self.pc/511e-6, 'brho':10*self.pc/2.9979, 'Ib':5.4, 'tau_BSP':3396, 'sigmPBS':0.192, 'sigmPSR':0.150 }
+                      'sigStY':68e-9, 'Lrnt':self.pc/511e-6, 'brho':10*self.pc/2.9979, 'Ib':5.4, 'tau_BSP':3396, 
+                      'sigmP':0.192 if self.SRorBS == 'BS' else 0.150,
+                      'V_RF':6.9e9 if self.fRF == 800e6 else 4.0e9 }
 
         print("---- Beam Parameters b1 at", self.pc, " GeV ---- \n", " Beam energy =", self.pc, 'with', param['Nbun'], 
               "bunches/beam with population of", param['Np'], 'particles \n', 
-              "bunch length (SR) =", param['sigZ'], "m \n",
-              "bunch length (BS) =", param['sigZ_BS'], "m \n",
-              "energy spread (SR) =", param['sigmPSR'], "%\n"
-              "energy spread (BS) =", param['sigmPBS'], '%\n',
+              "bunch length =", param['sigZ'], "m \n",
+              "energy spread =", param['sigmP'], "%\n"
             "\n emittance x,y =", param['epsX'], param['epsY'], "m \n",
             "\n Brho =", param['brho'], "\n Lorentz gamma =", param['Lrnt'], 
             "\n ----------------------------- " )
